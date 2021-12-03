580df0967f47d8cdefdb8de7fb72709ea6b49b09
Vivlo <jm.vivlo@gmail.com>
2021-12-03 17:47:41 -0600

    Adición de rutas y modificación al autor

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
-
 router.get('/', (req, res) => {
   res.send('sin f5');
 });
-
+router.get('/login', (req, res) => {
+  res.send('Dentro de Login');
+});
+router.get('/logout', (req, res) => {
+  res.send('Ahora Logout');
+});
 module.exports = router;
